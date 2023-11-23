7d03a6091d3e9e0e1ace5164afa4df592dd65dce
y0329 <kida_hirokazu@tuta.io>
2023-11-23 12:27:50 +0900

    [Add]ボタンの要素をヘッダーに移動

--- src/routes/(root)/+layout.svelte
+++ src/routes/(root)/+layout.svelte
@@ -1,9 +1,7 @@
 <script>
     import Header from "./Header.svelte";
     import ResultTable from "./ResultTable.svelte";
-    import StartAssebleButton from "./StartAssebleButton.svelte";
 </script>
 <Header />
 <ResultTable />
-<StartAssebleButton />
 <slot></slot>
