41e4623aab19a08db2c241257fb9a68a98379a34
kida_hirokazu <kida_hirokazu@tuta.io>
2023-12-04 00:18:50 +0900

    [Fix]肩武装でハンガーが選択できない問題を修正

--- src/routes/(root)/ResultTable.svelte
+++ src/routes/(root)/ResultTable.svelte
@@ -1,24 +1,32 @@
 <script>
-    import frame from "$lib/parts/frame.json";
-    import weapon from "$lib/parts/weapon.json";
-    import { fixedParts,result} from "$lib/store.js";
-    const type = ["種類", "パーツ", "固定"];
-    const orderTranslation = {
-    右腕武装: "double-hand-weapon",
-    左腕武装: "hand-weapon",
-    右肩武装: "double-shoulder-weapon",
-    左肩武装: "shoulder-weapon",
-    頭部: "head",
-    コア: "core",
-    腕部: "arm",
-    脚部: "reg",
-    ブースター: "booster",
-    火器管制: "fcs",
-    ジェネレータ: "generator",
-    拡張機能: "option",
+  import frame from "$lib/parts/frame.json";
+  import weapon from "$lib/parts/weapon.json";
+  import { fixedParts, result } from "$lib/store.js";
+  const type = ["種類", "パーツ", "固定"];
+  console.log(weapon["double-hand-weapon"], frame);
+  const orderTranslation = {
+    右腕武装: weapon["double-hand-weapon"],
+    左腕武装: weapon["hand-weapon"],
+    右肩武装: weapon["double-shoulder-weapon"].concat(
+      weapon["double-hand-weapon"].filter(function (parts) {
+        return parts["name"] != "NONE";
+      }),
+    ),
+    左肩武装: weapon["shoulder-weapon"].concat(
+      weapon["hand-weapon"].filter(function (parts) {
+        return parts["name"] != "NONE";
+      }),
+    ),
+    頭部: frame["head"],
+    コア: frame["core"],
+    腕部: frame["arm"],
+    脚部: frame["reg"],
+    ブースター: frame["booster"],
+    火器管制: frame["fcs"],
+    ジェネレータ: frame["generator"],
+    拡張機能: frame["option"],
   };
   const orders = Object.keys(orderTranslation);
-  const allDatas = Object.assign(frame, weapon);
   const parameter = ["重量", "EN負荷"];
   function hiddenswitch(event, elementId) {
     if (event.srcElement.checked) {
@@ -33,69 +41,75 @@
       event.srcElement.value;
   }
 </script>
+
 <table class="table table-sm">
-    <thead>
+  <thead>
+    <tr>
+      {#each type as column}
+        <th>{column}</th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody>
+    {#each orders as order}
       <tr>
-        {#each type as column}
-          <th>{column}</th>
-        {/each}
-      </tr>
-    </thead>
-    <tbody>
-      {#each orders as order}
-        <tr>
-          <td>
-            {order}
-          </td>
-          <td>
-            <select
-              id={orders.indexOf(order)}
-              class="select select-ghost w-full"
-              on:change={(event) =>
-                changeFixedParts(event, orders.indexOf(order))}
-              disabled
-            >
-              {#each allDatas[orderTranslation[order]] as categoryParts}
+        <td>
+          {order}
+        </td>
+        <td>
+          <select
+            id={orders.indexOf(order)}
+            class="select select-ghost w-full"
+            on:change={(event) =>
+              changeFixedParts(event, orders.indexOf(order))}
+            disabled
+          >
+            {#each [orderTranslation[order]] as categoryParts}
+              {#each categoryParts as parts}
                 {#if $result.length > 0}
-                  {#if categoryParts["name"] == $result[0][orders.indexOf(order)]["name"]}
-                    <option value={categoryParts["name"]} selected>{categoryParts["name"]}</option>
+                  {#if parts["name"] == $result[0][orders.indexOf(order)]["name"]}
+                    <option value={parts["name"]} selected
+                      >{parts["name"]}</option
+                    >
                   {:else}
-                    <option value={categoryParts["name"]}>{categoryParts["name"]}</option>
+                    <option value={parts["name"]}>{parts["name"]}</option>
                   {/if}
                 {:else}
-                  <option value={categoryParts["name"]}>{categoryParts["name"]}</option>
+                  <option value={parts["name"]}>{parts["name"]}</option>
                 {/if}
               {/each}
-            </select>
-          </td>
-          <td>
-            <input
-              type="checkbox"
-              id={orders.indexOf(order)}
-              on:change={(event) => hiddenswitch(event, orders.indexOf(order))}
-              class="toggle"
-            />
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-  <table class="table table-sm">
-    <thead>
-      <tr>
-        {#each parameter as column}
-          <th>{column}</th>
-        {/each}
-      </tr>
-    </thead>
-    <tbody>
-      {#each $result.slice(1, 3) as row}
-        <td>
-          {row}
+            {/each}
+          </select>
         </td>
+        <td>
+          <input
+            type="checkbox"
+            id={orders.indexOf(order)}
+            on:change={(event) => hiddenswitch(event, orders.indexOf(order))}
+            class="toggle"
+          />
+        </td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+<table class="table table-sm">
+  <thead>
+    <tr>
+      {#each parameter as column}
+        <th>{column}</th>
       {/each}
-    </tbody>
-  </table>
+    </tr>
+  </thead>
+  <tbody>
+    {#each $result.slice(1, 3) as row}
+      <td>
+        {row}
+      </td>
+    {/each}
+  </tbody>
+</table>
+
 <style>
   th,
   td {
